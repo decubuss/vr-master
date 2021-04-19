29d24ba67d4f125330251c6cf2f091d4d5808ccf
Andrej <amastafev@stud.kpfu.ru>
2021-04-20 01:31:52 +0300

    TeethGrip added
    damn it , this one gotta work

---
+++ Assets/TeethGrip.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.Rendering.PostProcessing;
+
+public class TeethGrip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private SphereCollider sphere;
+    private InputDevice device;
+
+    [HideInInspector]
+    public GameObject grabbedGO;
+    public XRNode inputSource;
+
+    void Awake()
+    {
+        sphere = GetComponent<SphereCollider>();
+        device = InputDevices.GetDeviceAtXRNode(inputSource);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
